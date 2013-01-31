68781fb264b6bcc5801c6216f958b82f3285c3bb
JeadoKo <haibane84@gmail.com>
2013-01-31 23:25:24 +0900

    사용자 조회 추가
    사용자를 조회한다.
    우선 사용자 데이터는 소스에 넣어두었다.
    
    추후 서비스를 만들면서 서버와 통신 하도록 변경할 것이다.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function mainCtrl ($scope) {
+	var userList = [
+		{ name : "고재도", email : "haibane84@gmail.com", regDate : "2013-01-20" },
+		{ name : "고재도2", email : "haibane84@gmail.com", regDate : "2013-01-20" },
+		{ name : "고재도3", email : "haibane84@gmail.com", regDate : "2013-01-20" }
+	];
+
+	$scope.search = function() {
+		$scope.userList = userList;
+	};
+
+	$scope.insert = function() {
+		if($scope.userList !== undefined){
+			$scope.userList.push({});
+		}
+	};
+}

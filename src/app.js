ffe68fc3e09ae9319e260d33ba13fc7cb635c2b3
Jeado Ko <haibane84@gmail.com>
2013-02-01 10:53:32 +0900

    Module 도입
    UserMnge 모듈 도입
    
    Signed-off-by: Jeado Ko <haibane84@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
-function mainCtrl ($scope) {
+/**
+ * UserMnge Module
+ *
+ * Description
+ */
+angular.module('UserMnge', [])
+.controller('mainCtrl',function($scope) {
+
 	var userList = [
 		{ name : "고재도", email : "haibane84@gmail.com", regDate : "2013-01-20" },
 		{ name : "고재도2", email : "haibane84@gmail.com", regDate : "2013-01-20" },
@@ -10,9 +17,11 @@ function mainCtrl ($scope) {
 	};
 
 	$scope.insert = function() {
-		if($scope.userList !== undefined 
-			&& $scope.userList[$scope.userList.length-1].name !== undefined){
-			$scope.userList.push({});
+
+		if($scope.userList !== undefined &&
+			$scope.userList[$scope.userList.length-1].name !== undefined){
+				
+				$scope.userList.push({});
 		}
 	};
 
@@ -23,4 +32,40 @@ function mainCtrl ($scope) {
 	$scope.del = function(index) {
 		$scope.userList.splice(index, 1);
 	};
-}
+
+});
+
+/**
+ * UserMnge Module
+ *
+ * Description
+ */
+angular.module('UserMnge', []).
+	controller("controllerA",function($scope) {
+		// ...
+	}).
+	directive('directiveA', ['$rootScope', function($rootScope){
+		return {
+			link: function($scope, iElm, iAttrs, controller) {
+				// ...
+			}
+		};
+	}])
+	.filter('filterA',function() {
+		// ...
+	})
+	.factory('serviceA',function() {
+		return {
+			//...
+		};
+	});
+
+
+
+
+
+
+
+
+
+

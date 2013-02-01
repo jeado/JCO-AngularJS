9942ee768fd11e13ba60f0931048a8a8232dc45c
Jeado Ko <haibane84@gmail.com>
2013-02-01 17:28:00 +0900

    UserResource 서비스 추가
    UserResource를 DI로 주입
    
    MongoLab을 이용
    
    Signed-off-by: Jeado Ko <haibane84@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,71 +1,88 @@
 /**
  * UserMnge Module
- *
- * Description
  */
-angular.module('UserMnge', [])
-.controller('mainCtrl',function($scope) {
 
-	var userList = [
-		{ name : "고재도", email : "haibane84@gmail.com", regDate : "2013-01-20" },
-		{ name : "고재도2", email : "haibane84@gmail.com", regDate : "2013-01-20" },
-		{ name : "고재도3", email : "haibane84@gmail.com", regDate : "2013-01-20" }
-	];
+// var factory = {
+// 	getUserResource : function() {
+// 		return new UserResource(factory.getAjax(), factory.getJsonParser());
+// 	},
+// 	getAjax : function() {
+// 		return new Ajax();
+// 	},
+// 	getJsonParser : function() {
+// 		return new JsonPartser();
+// 	}
+// };
+
+angular.module('UserMnge', ['ngResource'])
+.controller('mainCtrl',function($scope, UserResource) {
+
+	// var userResource = factory.getUserResource();
+
+	// var userResource = new UserResource(new Ajax(), new JsonParser());
+
+	// var userList = [
+	// 	{ name : "고재도", email : "haibane84@gmail.com", regDate : "2013-01-20" },
+	// 	{ name : "고재도2", email : "haibane84@gmail.com", regDate : "2013-01-20" },
+	// 	{ name : "고재도3", email : "haibane84@gmail.com", regDate : "2013-01-20" }
+	// ];
 
 	$scope.search = function() {
-		$scope.userList = userList;
+		var searchName = ($scope.searchName === "") ? undefined : $scope.searchName;
+		var query=JSON.stringify({ name : searchName });
+
+		$scope.userList = UserResource.query({q : query});
 	};
 
 	$scope.insert = function() {
-
-		if($scope.userList !== undefined &&
-			$scope.userList[$scope.userList.length-1].name !== undefined){
-				
-				$scope.userList.push({});
+		if($scope.userList !== undefined){
+			if($scope.userList[$scope.userList.length-1] === undefined){
+				$scope.userList.push({ edit : true });
+			}else if($scope.userList[$scope.userList.length-1].name !== undefined){
+				$scope.userList.push({ edit : true });
+			}
 		}
 	};
 
 	$scope.edit = function(user) {
 		user.edit = (user.edit) ? false : true;
+
+		if(user.edit === false){
+			if(user instanceof UserResource){
+				user.$update();
+			}else {
+				new UserResource(user).$save();
+			}
+		}
 	};
 
 	$scope.del = function(index) {
+		var user = $scope.userList[index];
+		
+		if(user instanceof UserResource){
+			user.$remove();
+		}
+		
 		$scope.userList.splice(index, 1);
 	};
 
-});
+})
+.value('mongolabAPIKey','50728d46e4b088be4c29ea02')
+.factory('UserResource',function($resource, mongolabAPIKey) {
 
-/**
- * UserMnge Module
- *
- * Description
- */
-angular.module('UserMnge', []).
-	controller("controllerA",function($scope) {
-		// ...
-	}).
-	directive('directiveA', ['$rootScope', function($rootScope){
-		return {
-			link: function($scope, iElm, iAttrs, controller) {
-				// ...
-			}
-		};
-	}])
-	.filter('filterA',function() {
-		// ...
-	})
-	.factory('serviceA',function() {
-		return {
-			//...
-		};
+	var User = $resource('https://api.mongolab.com/api/1/databases/jco-demo/collections/users/:id',{
+		apiKey : mongolabAPIKey
+	},{
+		update : {method:'PUT'}
 	});
 
+	User.prototype.$remove = function(){
+		User.remove({id: this._id.$oid});
+	};
 
+	User.prototype.$update = function() {
+		return User.update({id: this._id.$oid},angular.extend({},this,{_id: undefined }));
+	};
 
-
-
-
-
-
-
-
+	return User;
+});
